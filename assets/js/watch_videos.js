2f61bbc0e154b87dd50f1870a40c79ffc5518b40
realtyme <38818454+realtyme@users.noreply.github.com>
2023-08-22 13:37:30 +0300

    added the youtube script

--- assets/js/watch_videos.js
+++ assets/js/watch_videos.js
@@ -1,3 +1,25 @@
+// Augmented Reality
+var africa_ar_btn = document.getElementById('africa-ar')
+africa_ar_btn.addEventListener('click', function(e){
+    videoSrc = africa_ar_btn.getAttribute('data-bs-src')
+})
+
+var turkana_btn = document.getElementById('turkana-boy')
+turkana_btn.addEventListener('click', function(e){
+    videoSrc = turkana_btn.getAttribute('data-bs-src')
+})
+
+var sogallo_btn = document.getElementById('sogallo-ar')
+sogallo_btn.addEventListener('click', function(e){
+    videoSrc = sogallo_btn.getAttribute('data-bs-src')
+})
+
+var abwao_btn = document.getElementById('abwao-kraal')
+abwao.addEventListener('click', function(e){
+    videoSrc = abwao.getAttribute('data-bs-src')
+})
+
+// Virtual Reality
 var ndw_btn = document.getElementById('nairobi-design-week')
 ndw_btn.addEventListener('click', function(e){
     videoSrc = ndw_btn.getAttribute('data-bs-src')
@@ -7,8 +29,6 @@ var wstrn_btn = document.getElementById('wstrn-unload')
 wstrn_btn.addEventListener('click', function(e){
     videoSrc = ndw_btn.getAttribute('data-bs-src')
 })
-
-
 
 
 var videoModal = document.getElementById('modal-youtube-videos')
